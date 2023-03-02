19049bc3e6316ac73de02d4f30f431948e3290f0
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-03-02 12:05:16 -0300

    Ejercicio 11 Realizado

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -3,3 +3,24 @@
 
 Nota: ver funcion Math() https://www.w3schools.com/js/js_math.asp
  */
+
+ let nombre1 = prompt("Nombre 1:");
+ let edad1 = parseInt(prompt("Edad 1:"));
+
+ let nombre2 = prompt("Nombre 2:");
+ let edad2 = parseInt(prompt("Edad 2:"));
+
+ let nombre3 = prompt("Nombre 3:");
+ let edad3 = parseInt(prompt("Edad 3:"));
+
+ let maximo = Math.max(edad1, edad2, edad3);
+
+ if (maximo === edad1) {
+     document.write("El mayor es: " + nombre1);
+ }
+ else if (maximo === edad2) {
+     document.write("El mayor es: " + nombre2);
+ }
+ else if (maximo === edad3) {
+     document.write("El mayor es: " + nombre3);
+ }
