331c04cf015028f35b4054167979ac928e892fbe
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-03-03 21:45:38 -0300

    Ejercicio 13 Realizado

--- js/ejercicio13.js
+++ js/ejercicio13.js
@@ -1,3 +1,7 @@
 /*
 13- Realiza un script que pida un texto y lo muestre en mayúsculas.
  */
+
+let texto = prompt("Ingrese un texto: ")
+
+document.write(`El texto en mayúsculas quedaría así: "${texto.toUpperCase()}"`)
